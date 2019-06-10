4937ce918e1bf9431d6a6a1856384d96cc5dfb6e
Max <maxmillianrwoods@gmail.com>
2019-06-10 14:03:50 -0700

    setup in progress

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 // TODO: Render the `App` component to the DOM
 import App from './components/App.js';
+import YOUTUBE_API_KEY from './config/youtube.js';
+import searchYouTube from './lib/searchYouTube.js';
+
+ReactDOM.render(<App />, document.getElementById('app'));
 
